b64bbe55fbc24d8c8fdd4c96c349ccc344b1a9a1
Tapas Pradhan <pradhanta20@gmail.com>
2023-09-21 16:36:31 +0530

    To day I start a Password Generator Project

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState,useCallback } from 'react'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed,setNumberAllowed]=useState(false)
+  const [charAllowed,setCharAllowed]=useState(false)
+  const [password,setPassword]=useState("")
+
+  const passwordGenerator=useCallback(()=>{
+    const pass=""
+    const str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllowed)str+="0123456789"
+    if(charAllowed)str+="!@#$%^&*()~{}[]`"
+
+    for (let i=1; i<=array.length; i++){
+      let char=Math.floor(Math.random() * str.length+1)
+      pass=str.charAt(char)
+    }
+    setPassword(pass)
+
+  },[length,numberAllowed,charAllowed,setPassword])
+
+  }
+
+  return (
+    <>
+     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-500'>
+      <h1 className='text-white text-center my-3'>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input
+        type="text"
+        value={password}
+        className='outline-none w-full py-1 px-3'
+        placeholder='Password'
+        readOnly
+        />
+        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+      </div>
+      <div className="flex text-sm gap-x-2">
+        <div className='flex items-center gap-x-1'>
+          <input
+          type="range"
+          min={6}
+          max={100}
+          value={length}
+          className='cursor-pointer'
+          onChange={(e)=>{setLength(e.target.value)}}
+          />
+          <label htmlFor="">Length:{length}</label>
+
+        </div>
+        <div className="flex item-center gap-x-1">
+          <input
+            type="checkbox" 
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={()=>{
+              setNumberAllowed((prev)=>!prev);
+            }}
+          />
+        </div>
+        <label htmlFor="numberInput">Numbers</label>
+      </div>
+     </div>
+    </>
+  )
+
+
+export default App

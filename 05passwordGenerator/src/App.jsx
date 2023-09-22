d9814b73d37b05a915e49371b41c9243ef252c06
Tapas Pradhan <pradhanta20@gmail.com>
2023-09-22 10:07:49 +0530

    Today I use more react tool on this project

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useCallback } from 'react'
+import { useState,useCallback,useEffect,useRef } from 'react'
 
 function App() {
   const [length, setLength] = useState(8)
@@ -6,22 +6,33 @@ function App() {
   const [charAllowed,setCharAllowed]=useState(false)
   const [password,setPassword]=useState("")
 
+  // useRef
+  const passwordRef=useRef(null)
+
+
   const passwordGenerator=useCallback(()=>{
-    const pass=""
-    const str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if(numberAllowed)str+="0123456789"
     if(charAllowed)str+="!@#$%^&*()~{}[]`"
 
-    for (let i=1; i<=array.length; i++){
-      let char=Math.floor(Math.random() * str.length+1)
-      pass=str.charAt(char)
+    for (let i=1; i<=length; i++){
+      let char=Math.floor(Math.random() * str.length + 1)
+      pass +=str.charAt(char)
     }
     setPassword(pass)
 
   },[length,numberAllowed,charAllowed,setPassword])
 
-  }
+  const passwordToClipboard=useCallback(()=>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,99);
+    window.navigator.clipboard.writeText(password)
+  },[password])
 
+  useEffect(()=>{
+    passwordGenerator()
+  },[length,numberAllowed,charAllowed,passwordGenerator])
   return (
     <>
      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-500'>
@@ -33,8 +44,11 @@ function App() {
         className='outline-none w-full py-1 px-3'
         placeholder='Password'
         readOnly
+        ref={passwordRef}
         />
-        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+        <button 
+        onClick={passwordToClipboard}
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
       </div>
       <div className="flex text-sm gap-x-2">
         <div className='flex items-center gap-x-1'>
@@ -61,9 +75,23 @@ function App() {
         </div>
         <label htmlFor="numberInput">Numbers</label>
       </div>
+      <div className='flex items-center gap-x-1'>
+            <input
+              type="checkbox" 
+              defaultChecked ={charAllowed}
+              id='characterInput'
+              onChange={()=>{
+                setCharAllowed((prev)=>!prev)
+              }}
+              />
+              <label htmlFor="characterInput">Characters</label>
+      </div>
+
      </div>
     </>
   )
+  }
+
 
 
 export default App

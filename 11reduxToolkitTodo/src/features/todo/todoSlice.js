b390f2bf734529a476844e0eb25d2271807fd91c
Tapas Pradhan <pradhanta20@gmail.com>
2023-09-30 22:13:48 +0530

    To day I start a new project thats's name is Blog Post Website

--- 11reduxToolkitTodo/src/features/todo/todoSlice.js
+++ 11reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -3,6 +3,7 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState={
     todos:[{id:1,text:"Hello World"}]
+
 }
 
 
@@ -16,7 +17,7 @@ export const todoSlice=createSlice({
                 id:nanoid(),
                 text:action.payload
             }
-            state.todos.push(todo)
+        state.todos.push(todo)
         },
         removeTodo:(state,action)=>{
             state.todos=state.todos.filter((todo)=>todo.id !==action.payload)
---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.VITE_APPWRITE_URL);
+
+  return (
+    <>
+    <div className="w-full bg-orange-700 w-100%">
+      <h1 className="text-3xl block  text-white ">Creating A Blog Posting Website</h1>
+
+    </div>
+    <div className="w-full bg-orange-700 w-100%">
+      <h1 className="text-3xl block  text-white ">Creating A Blog Posting Website</h1>
+
+    </div>
+    </>
+  )
+}
+
+export default App
